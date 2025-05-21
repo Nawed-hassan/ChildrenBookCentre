4687046844b3b51ac3fffec643665d7374e08a0f
Nawed-hassan <hassannawed474@gmail.com>
2025-05-21 22:10:24 +0530

    api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Optional: ensure consistent base URL
+  baseURL: 'https://childrenbookcentre-backend.onrender.com/api', // Optional: ensure consistent base URL
   withCredentials: true,
 });
 
